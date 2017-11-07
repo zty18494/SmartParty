1a7a47090924a3aadea2c33aa9f7ab94ed60e11e
Elsie.Zhang <zty18945@163.com>
2017-11-07 14:32:56 +0800

    Update doc_parser.py
    增加searchFromES2方法，使用整句查询，提高答案匹配正确性；
    在查询和比较答案时去除了字符串中的标点和html占位符；

--- doc_parser.py
+++ doc_parser.py
@@ -9,8 +9,10 @@ import time
 import re
 # import httplib    #python 2.7 
 import http.client as httplib  #python 3
+import operator #python3
 import urllib
 import json
+import string
 
 '''
 题目类型
@@ -247,10 +249,10 @@ def putIntoES(knowledge, ip = '172.23.27.171', port = 9200, index = 'party', typ
         print ('[storeIntoES] the Knowledge is invalid! info:', knowledge.getAllInfo())
         return False
 
-    
 # 从ES中查找答案
 def searchFromES(question, ip = '172.23.27.171', port = 9200, index = 'party', type = 'smart'):
     if question:
+        question = removeHTMLSpace(question)
         json_data = {"query" : {"match" : {"question" : question}}}
         data = json.dumps(json_data) # Encode the json_data
         
@@ -284,6 +286,58 @@ def searchFromES(question, ip = '172.23.27.171', port = 9200, index = 'party', t
     else:
         print ('[searchFromES] the question is null!')
         return None
+    
+
+# 从ES中查找答案，使用整句查询
+'''
+{
+      "query": {
+        "multi_match" : {
+            "query": "党员领导干部违反有关规定干预和插手（ ）等活动，造成重大损失或者不良影响的，给予警告或者严重警告处分;情节较重的，给予撤销党内职务或者留党察看处分;情节严重的，给予开除党籍处分。",
+            "fields": ["question"],
+            "type": "phrase",
+            "slop": 3
+        }
+    }
+}
+'''
+def searchFromES2(question, ip = '172.23.27.171', port = 9200, index = 'party', type = 'smart'):
+    if question:
+        question = removeHTMLSpace(question)
+        json_data = {"query" : {"multi_match" : {"query" : question, "fields": ["question"], "type": "phrase", "slop": 3}}}
+        data = json.dumps(json_data) # Encode the json_data
+        
+        headers = {"Content-type": "text/plain;charset=UTF-8"}
+        
+        url = '/' + index + '/' + type + '/_search'
+        conn = httplib.HTTPConnection(ip, port)
+        
+        conn.request('POST', url, data, headers)
+        httpres = conn.getresponse()
+        
+        if httpres.status != 200:
+            print ('[searchFromES2] return not ok,', httpres.status, httpres.reason)
+            conn.close()
+            return None
+        else:
+            resdata = httpres.read()
+            conn.close()
+#             print ('[searchFromES2] return ok')
+            result = json.loads(resdata)   # dict
+            hits = result['hits']
+            total = hits['total']
+            sub_hits = hits['hits']
+            source = sub_hits[0]['_source']
+            question = source['question']
+            answer = source['answer']
+            type = int(source['model'])
+            knowledge = Knowledge(question, type, answer)
+            print ('[searchFromES2] parse the answer:', knowledge.getAllInfo())
+            return knowledge
+    else:
+        print ('[searchFromES2] the question is null!')
+        return None
+    
 '''
 [
 {question: [answers]},
@@ -295,6 +349,22 @@ def searchList(questions):
         if pair:
             pair.autoGetAnswers()
     return
+
+
+# 将字符串中的&nbsp;及其前面的字符去掉
+def removeHTMLSpace(text):    
+    if text:
+        index = text.rfind('&nbsp;')
+        if index >= 0:
+            return text[index + len('&nbsp;'):]
+    return text
+
+myPunctuation = string.punctuation + ' ，。、《》？；‘’：“”【】{}！@#￥%……&*（）——~'
+
+# 将字符串中的中英文标点去除并返回
+def removePunctuation(text):
+    return text.translate(str.maketrans('','',myPunctuation))
+
 
 '''
 寻找答案的问题类
@@ -310,9 +380,9 @@ class Question():
     submit_judge = ''   #判断题答案
     unfound_list = []   #没能找到序号的答案
     
-    def __init__(self, question = '', list = []):
+    def __init__(self, question = ''):
         self.question = question
-        self.options = list
+        self.options = ['', '', '', '', '']
         self.question_type = ''
         self.rst_answers = []
         self.rst_type = KnowledgeType.INVALID
@@ -320,8 +390,11 @@ class Question():
         self.submit_judge = ''
         self.unfound_list = []
     
+    '''
+    从知识库找到正确的答案字符串
+    '''
     def autoGetAnswers(self):
-        knowledge = searchFromES(self.question)
+        knowledge = searchFromES2(self.question)
         if knowledge:
             self.rst_type = knowledge.type
             if knowledge.type is KnowledgeType.MULTI_CHOICE:
@@ -338,20 +411,23 @@ class Question():
             print ('[autoGetAnswers] cannot get the ansewr of', self.question)
             return False
 
+    '''
+    将答案字符串与选项匹配，得出答案选项，或记录未找到的正确答案
+    '''
     def findSubmitAnswers(self):
         if self.rst_answers is None or self.options is None:
             print ('[findSubmitAnswers] answers or options is none!')
             return
         
         if self.rst_type in [KnowledgeType.MULTI_CHOICE, KnowledgeType.SINGLE_CHOICE]:
+            
             for r in self.rst_answers:
                 found = False
-                o_index = 0
-                for o in self.options:
-                    o_index += 1
-                    if o and o.find(r):
-                        
-                        self.submit_choices.append(char(64 + o_index))
+                o_index = -1
+                for i in range(0, len(self.options) - 1):
+                    if operator.eq(removePunctuation(self.options[i]), removePunctuation(r)):               ############ TODO:
+                        self.submit_choices.append(chr(97 + i))
+                        print('(', str(i), ')find choice:', chr(97 + i))
                         found = True
                         break
                     
@@ -361,10 +437,25 @@ class Question():
         elif self.rst_type is KnowledgeType.JUDGE:
             self.submit_judge = self.rst_answers[0]
         
-        print('[findSubmitAnswers] submit_choice:', ', '.join(self.submit_choices), 
-              ', submit_judge:', self.submit_judge, 
-              ', unfound_answers:', '; '.join(self.unfound_list), '\n\n')
+        print('[findSubmitAnswers] submit_choice:', self.getSubmitAnsersString(), '\n\n')
 
+    '''
+    组装出最终答案选项
+    '''
+    def getSubmitAnsersString(self):
+        s = ''
+        if self.rst_type is KnowledgeType.JUDGE:
+            s = self.submit_judge;
+        elif self.rst_type in [KnowledgeType.MULTI_CHOICE, KnowledgeType.SINGLE_CHOICE]:
+            self.submit_choices.sort()
+            s = ''.join(self.submit_choices)
+            if len(self.unfound_list) > 0:
+                s += '，补充答案：'
+                for unfound in self.unfound_list:
+                    s += '； '.join(self.unfound_list)
+                    
+        return s
+        
 if __name__ == '__main__':
 #     parseMultiChoiceKnowledgeFromDoc('./test1', True)
 #     parseJudgeKnowledgeFromDoc('./test2', True)
